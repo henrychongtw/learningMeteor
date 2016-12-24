9a0d9d63feeceb946e563045a2f577f2864fdf9e
Henry Chong <huashiuanchong@gmail.com>
2016-12-25 07:00:20 +0800

    navigation to different url

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Bins } from '../imports/collections/bins';
-
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
 
 import App from './components/app';
 
 const routes = (
 	<Router history={browserHistory}>
 		<Route path="/" component={App}>
+			<IndexRoute component={BinsList} />
+			<Route path='bins/:binId' component={BinsMain} />
+		}
 		</Route>
 	</Router>
 );

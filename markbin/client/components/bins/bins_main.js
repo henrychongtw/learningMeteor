dc3c467d4664c6266d7f877177713dabd7e8ed0b
Henry Chong <huashiuanchong@gmail.com>
2016-12-25 08:58:18 +0800

    finish editor with saving function

--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
 
 class BinsMain extends Component {
 	render() {
-		// console.log(this.props.params.binId);
+		// console.log(this.props.bin);
+		if(!this.props.bin) { return <div>Loading...</div>}
+
 		return (
-			<div>BinsMain</div>
+			<div>
+				<BinsEditor bin={this.props.bin} />
+			</div>
 		);
 	}
 }
 
-export default BinsMain;
+export default createContainer((props) => {
+	const { binId } = props.params;
+	Meteor.subscribe('bins');
+
+	return { bin: Bins.findOne(binId) };
+}, BinsMain);
